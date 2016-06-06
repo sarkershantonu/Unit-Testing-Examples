c0ee491b2850206f6c8359960a48b656c4e8ba18
sarkershantonu <shantonu_oxford@yahoo.com>
2016-06-06 02:06:50 +0200

    Example added from old code

---
+++ Junit-Examples/src/test/java/benchmark/JunitBenchMarkTests.java
@@ -0,0 +1,28 @@
+package benchmark;
+
+import com.carrotsearch.junitbenchmarks.AbstractBenchmark;
+import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
+import org.junit.After;
+import org.junit.Test;
+
+public class JunitBenchMarkTests extends AbstractBenchmark {
+	private int i = 0;
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() throws InterruptedException {
+		Thread.sleep(20);
+	}
+
+	@BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 1, concurrency = 3)
+	@Test
+	public void testWithBenchMark() throws InterruptedException {
+		System.out.println("Running " + i++);
+		Thread.sleep(20);
+		//http://labs.carrotsearch.com/junit-benchmarks-tutorial.html
+	}
+
+}
---
+++ Junit-Examples/src/test/java/benchmark/TempusTests.java
@@ -0,0 +1,9 @@
+package benchmark;
+
+import com.google.code.tempusfugit.concurrency.ConcurrentTestRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcurrentTestRunner.class)
+public class TempusTests {
+
+}
