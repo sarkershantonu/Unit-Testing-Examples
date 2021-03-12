92dc47f44e81989e9df56b86abd1ecb2e19d29ae
shantonu sarker <sarker.shantonu@gmail.com>
2021-03-13 01:33:03 +0600

    test project to examine testNG

--- TestNG-Examples/src/main/java/core/Calculator.java
+++ TestNG-Examples/src/main/java/core/Calculator.java
@@ -2,18 +2,18 @@ package core;
 
 public class Calculator {
 
-public long Add(int num2,int num3)
+public long add(int num2,int num3)
 {
 	return (long)(num2+num3);	
 }
 
-public long Add(int num1,int num2,int num3)
+public long add(int num1,int num2,int num3)
 {
-	long res = this.Add(num2,num3);
+	long res = this.add(num2,num3);
 	return (long)num1+res;	
 }
-
-public double Add(double num1,double num2,double num3)
+	public long add(long a, long b){return a+b;}
+public double add(double num1,double num2,double num3)
 {
 	return num1+num2+num3;	
 }
